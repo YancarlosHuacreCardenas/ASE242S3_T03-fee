d6a4bfc1c851a1a4c2b5f6d44643e6902e416dca
Yancarlos Huacre Cardenas <131498027+YancarlosHuacreCardenas@users.noreply.github.com>
2025-12-02 15:09:03 -0500

    Update customer.service.ts

--- src/app/services/customer.service.ts
+++ src/app/services/customer.service.ts
@@ -8,7 +8,7 @@ import { Customer } from "../models/customer.model";
 })
 export class CustomerService {
   // URL base apuntando al backend
-  private apiUrl = "http://44.197.16.32:8080/api/customers";
+  private apiUrl = "http://13.220.64.76:8080/api/customers";
 
   constructor(private http: HttpClient) {}
 
